7bb15a1e85577136dae0d91da5de9dbf72a4487c
HGSIE12 <xhmode_sa@hotmail.com>
2026-03-02 14:04:45 +0300

    Add files via upload

---
+++ Week7_Lab38.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+X = np.array([
+    [1.0, 2.0, 3.0],
+    [4.0, 5.0, 6.0],
+])
+W = np.array([
+    [0.1, -0.2],
+    [0.4, 0.0],
+    [-0.3, 0.5],
+])
+
+print("X shape: ", X.shape) # (3, 3)
+print("\nW shape: ", W.shape) # (3, 2)
+
+res = np.dot(X, W)
+print("\nres shape: ", res.shape) #(2, 2)
